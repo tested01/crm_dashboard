5f9311cb662a2e0948fcea9837a14335e79268f7
testudn01 <testudn1@gmail.com>
2017-05-31 16:24:26 +0800

    test commit

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>CRM Dashboard</div>
+      <div> CRM Dashboard </div>
     );
   }
 }
